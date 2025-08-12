756069204b7ecdd6ed64d77e3660dab2885e4907
Sergio <santossergiorealbjj@outlook.com>
2025-08-12 17:11:28 -0300

    Update TaskHistoryRepository.java

--- src/main/java/com/seu_pacote/repository/TaskHistoryRepository.java
+++ src/main/java/com/seu_pacote/repository/TaskHistoryRepository.java
@@ -6,6 +6,18 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+
+package com.seuprojeto.board.repository;
+
+import com.seuprojeto.board.model.TaskHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface TaskHistoryRepository extends JpaRepository<TaskHistory, Long> {
+    List<TaskHistory> findByTaskIdOrderByActionDateDesc(Long taskId);
+}
+
+
 @Repository
 public interface TaskHistoryRepository extends JpaRepository<TaskHistory, Long> {
     // Busca histórico por task ordenado por data (descendente ou ascendente)
